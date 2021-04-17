bab326670b2ed64f241d3b23cd6e1fa6aacb8401
Makarov Danil <ninjaheister@gmail.com>
2021-04-17 03:28:24 +0200

    printings

--- src/screens/CheckBoxBuilder/CheckBoxBuilder.js
+++ src/screens/CheckBoxBuilder/CheckBoxBuilder.js
@@ -1,10 +1,12 @@
-import { Checkbox, Divider, Grid, Typography } from '@material-ui/core';
+import { Button, Checkbox, Divider, Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import Header from '../../components/Header/Header';
-
-
+import ReactToPrint from 'react-to-print';
+import MyButton from '../../UI/Button/MyButton';
+import s from './CheckBoxBuilder.module.css'
 
 const CheckBoxBuilder = (props) => {
+  const componentRef = React.useRef();
   let countOfQuestions = null
   let countOfAnswers = null
   let nameOfTest = ""
@@ -16,31 +18,38 @@ const CheckBoxBuilder = (props) => {
   }
   console.log(props.location.state)
   return (
-    <Grid container direction="column">
+    <Grid container direction="column" >
       <Header />
-      <Grid item container direction="row" justify="center" style={{ marginTop: '100px' }}>
-        <Typography variant="body1">{nameOfTest}</Typography>
-      </Grid>
-      <Divider style={{ backgroundColor: 'black' }} />
-      <Grid item container direction="column">
-        <Grid item container direction="row" justify="center">
-          {Array.apply(null, { length: countOfAnswers }).map((elem, index) => {
-            return <Grid item container direction="row" justify="flex-end" style={{ width: '42px', paddingRight: '5px' }}>{ABC[index]}</Grid>
+      <Grid ref={componentRef} className={s.PrintArea}>
+        <Divider style={{ backgroundColor: 'black', width:'350px', margin:'100px auto 0 auto'}} />
+        <Grid item container direction="row" justify="center" style={{marginTop:'20px'}}>
+          <Typography variant="body1">{nameOfTest}</Typography>
+        </Grid>
+        <Grid item container direction="column" style={{marginTop:'80px'}}>
+          <Grid item container direction="row" justify="center">
+            {Array.apply(null, { length: countOfAnswers }).map((elem, index) => {
+              return <Grid item container direction="row" justify="flex-end" style={{ width: '42px', paddingRight: '5px' }}>{ABC[index]}</Grid>
+            })}
+          </Grid>
+          {Array.apply(null, { length: countOfQuestions }).map((elem, index) => {
+            return <>
+              <Grid item container direction="row" alignItems="center" justify="center">
+                <Grid item container direction="row" justify="flex-end" style={{ width: '23px' }}>
+                  <Typography variant="body1">{index + 1}</Typography>
+                </Grid>
+                {Array.apply(null, { length: countOfAnswers }).map(elem => {
+                  return <Checkbox />
+                })}
+              </Grid>
+            </>
           })}
         </Grid>
-        {Array.apply(null, { length: countOfQuestions }).map((elem, index) => {
-          return <>
-            <Grid item container direction="row" alignItems="center" justify="center">
-              <Grid item container direction="row" justify="flex-end" style={{ width: '23px' }}>
-                <Typography variant="body1">{index + 1}</Typography>
-              </Grid>
-              {Array.apply(null, { length: countOfAnswers }).map(elem => {
-                return <Checkbox />
-              })}
-            </Grid>
-          </>
-        })}
       </Grid>
+      <ReactToPrint
+        trigger={() => <MyButton name="Роздрукувати"></MyButton>}
+        content={() => componentRef.current}
+        pageStyle="А4"
+      />
     </Grid>
 
   )
