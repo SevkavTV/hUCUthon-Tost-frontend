380a1f6e798a8f2cb2578497a2df36a5f913f809
SevkavTV <seva.archakov@gmail.com>
2021-04-17 07:40:44 +0300

    photos download

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Register from './screens/Landing/Register/Register';
 import { Route, Switch } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import CheckBoxBuilder from './screens/CheckBoxBuilder/CheckBoxBuilder';
+import ChooseTemplate from './screens/ChooseTemplate/ChooseTemplate'
+import Results from './screens/Results/Results'
 
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <Switch>
           <PrivateRoute exact path="/" component={Landing} />
           <PrivateRoute exact path="/template" component={CheckBoxBuilder} />
+          <PrivateRoute exact path="/choose_template" component={ChooseTemplate} />
+          <PrivateRoute exact path="/results" component={Results} />
           <Route exact path="/login" component={Register} />
         </Switch>
     </div>
